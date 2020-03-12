074df8c43b8962444f0b60a002a63b1521614bf9
hayaAbdulaziz <hayal5066@gmail.com>
2020-03-12 16:48:44 +0300

    UserModel

---
+++ back-end/model/User.js
@@ -0,0 +1,10 @@
+// Require necessary NPM packages
+const mongoose = require('mongoose');
+// Define Article Schema
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+});
+// Compile our Model based on the Schema
+const User = mongoose.model('User', userSchema);
+// Export our Model for use
+module.exports = User;
