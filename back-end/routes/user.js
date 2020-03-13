a658a38c01bb65c05368d99e755cc43661a72f7a
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-03-13 20:27:32 +0300

    Add Create User Action

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -2,19 +2,23 @@
 const express = require("express");
 
 //Require Mongoose Model for Users
-const User =require('../model/User')
+const User = require("../model/User");
 
 //Instantiate a Router (min app that only handles routes)
-const router =express.Router();
+const router = express.Router();
 
+/**
+ * @method POST
+ * @route   /api/users
+ * @action  CREATE
+ * @desc    Create a new user
+ */
+router.post("/api/users", (req, res) => {
+    // Add the user recieved from the request body to the database
+    User.create(req.body.user)
+        .then(user => res.status(201).json({ user }))
+        .catch(error => res.status(500).json({ error }));
+});
 
-
-
-
-
-
-
-
-//export the Router 
-module.exports=router;
-
+//export the Router
+module.exports = router;
