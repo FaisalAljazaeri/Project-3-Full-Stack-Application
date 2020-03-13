d7383f8b0f85e674543610c930c6a5046d3f20eb
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-03-13 20:31:20 +0300

    Add Update User Action

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -20,5 +20,36 @@ router.post("/api/users", (req, res) => {
         .catch(error => res.status(500).json({ error }));
 });
 
+/**
+ * @method PATCH
+ * @route   /api/users/id
+ * @action  UPDATE
+ * @desc    Update a user by ID
+ */
+router.patch("/api/users/:id", (req, res) => {
+    // Find the user with the passed ID
+    User.findById(req.params.id)
+        .then(user => {
+            // Check if a user is found by the passed ID
+            if (user) {
+                // Update the existing user with the new data from the request body
+                return user.update(req.body.user);
+            } else {
+                // If no user was found by the passed ID, send an error message as response
+                res.status(404).json({
+                    error: {
+                        name: "DocumentNotFoundError",
+                        message: "The provided ID doesn't match any documents"
+                    }
+                });
+            }
+        })
+        .then(() => {
+            // If the update succeeded, return 204 and no JSON response
+            res.status(204).end();
+        })
+        .catch(error => res.status(500).json({ error }));
+});
+
 //export the Router
 module.exports = router;
