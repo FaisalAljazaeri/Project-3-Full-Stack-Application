6587e50ef7cc1a28832a315948d525011e1d7be2
Ranen Alkhlabi <Ranen-Alhklabi@hotmail.com>
2020-03-14 03:02:00 +0300

    Add Create Organization Action

--- back-end/routes/organization.js
+++ back-end/routes/organization.js
@@ -2,13 +2,26 @@
 const express = require("express");
 
 //Require Mongoose Model for Organization
-const Organization =require('../model/Organization')
+const Organization = require('../model/Organization')
 
 //Instantiate a Router (min app that only handles routes)
-const router =express.Router();
+const router = express.Router();
 
 
+  
 
+/**
+ * @method POST
+ * @route   /api/organizations
+ * @action  CREATE
+ * @desc    Create a new organizations
+ */
+router.post("/api/organizations", (req, res) => {
+    // Add the organizations recieved from the request body to the database
+    Organization.create(req.body.organization)
+        .then(organization => res.status(201).json({ organization }))
+        .catch(error => res.status(500).json({ error }));
+});
 
 
 
@@ -16,5 +29,4 @@ const router =express.Router();
 
 
 //export the Router 
-module.exports=router;
-
+module.exports = router;
