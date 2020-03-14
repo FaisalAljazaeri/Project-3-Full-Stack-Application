6f61b5827cef54bec32bea693f594ae7cb0340f7
wejdann <weejdansaud58@hotmail.com>
2020-03-14 14:23:44 +0300

    Create end point to delete user by id

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -57,10 +57,10 @@ router.get("/api/user/:id", (req, res) => {
  * @desc    Create a new user
  */
 router.post("/api/users", (req, res) => {
-    // Add the user recieved from the request body to the database
-    User.create(req.body.user)
-        .then(user => res.status(201).json({ user }))
-        .catch(error => res.status(500).json({ error }));
+  // Add the user recieved from the request body to the database
+  User.create(req.body.user)
+    .then(user => res.status(201).json({ user }))
+    .catch(error => res.status(500).json({ error }));
 });
 
 /**
@@ -70,28 +70,59 @@ router.post("/api/users", (req, res) => {
  * @desc    Update a user by ID
  */
 router.patch("/api/users/:id", (req, res) => {
-    // Find the user with the passed ID
-    User.findById(req.params.id)
-        .then(user => {
-            // Check if a user is found by the passed ID
-            if (user) {
-                // Update the existing user with the new data from the request body
-                return user.update(req.body.user);
-            } else {
-                // If no user was found by the passed ID, send an error message as response
-                res.status(404).json({
-                    error: {
-                        name: "DocumentNotFoundError",
-                        message: "The provided ID doesn't match any documents"
-                    }
-                });
-            }
-        })
-        .then(() => {
-            // If the update succeeded, return 204 and no JSON response
-            res.status(204).end();
-        })
-        .catch(error => res.status(500).json({ error }));
+  // Find the user with the passed ID
+  User.findById(req.params.id)
+    .then(user => {
+      // Check if a user is found by the passed ID
+      if (user) {
+        // Update the existing user with the new data from the request body
+        return user.update(req.body.user);
+      } else {
+        // If no user was found by the passed ID, send an error message as response
+        res.status(404).json({
+          error: {
+            name: "DocumentNotFoundError",
+            message: "The provided ID doesn't match any documents"
+          }
+        });
+      }
+    })
+    .then(() => {
+      // If the update succeeded, return 204 and no JSON response
+      res.status(204).end();
+    })
+    .catch(error => res.status(500).json({ error }));
+});
+/**
+ * @method  : delete
+ * @route   : /api/users/id
+ * @action  : Destory
+ * @desc    : delete an user by user ID
+ */
+router.delete("/api/user/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        //pass the result of mongooes's ".delete" method to thee next ".then"
+        user.remove();
+      } else {
+        // if we coudn;t find a document with matching ID
+        res.status(404).json({
+          error: {
+            name: "DocumentNotFoundError ",
+            message: "The  providednId dosen't match any documents"
+          }
+        });
+      }
+    })
+    //another then
+    .then(() => {
+      // if the deletion succeeded , return 204 and no JSON
+      res.status(204).end();
+    })
+    .catch(error => {
+      res.status.json({ error: error });
+    });
 });
 
 //export the Router
