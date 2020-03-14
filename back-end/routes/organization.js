399893a7c1b7312cecadc75450527f59b05350b2
Ranen Alkhlabi <Ranen-Alhklabi@hotmail.com>
2020-03-14 03:03:34 +0300

    Add Delete Organization Action

--- back-end/routes/organization.js
+++ back-end/routes/organization.js
@@ -25,6 +25,39 @@ router.post("/api/organizations", (req, res) => {
 
 
 
+/**
+ * @method DELETE
+ * @route   /api/organizations/id
+ * @action  DESTROY
+ * @desc    Delete An organization by organization ID
+ */
+router.delete("/api/organizations/:id", (req, res) => {
+    // Find the organization with the passed ID
+    Organization.findById(req.params.id)
+        .then(organization => {
+            // Check if a organization is found by the passed ID
+            if (organization) {
+               // pass the result of Mongoose's  .delete method to next.then statment
+                return organization.delete();
+            } else {
+                // If no user was found by the passed ID, send an error message as response
+                res.status(404).json({
+                    error: {
+                        name: "DocumentNotFoundError",
+                        message: "The provided ID doesn't match any documents"
+                    }
+                });
+            }
+        })
+        .then(() => {
+            // If the update succeeded, return 204 and no JSON response
+            res.status(204).end();
+        })
+        .catch(error => res.status(500).json({ error }));
+});
+
+
+
 
 
 
