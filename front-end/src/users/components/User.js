72c74f2d55a6bae0a1fa66da23ad326da2148a35
wejdann <weejdansaud58@hotmail.com>
2020-03-16 01:30:27 +0300

    Find the names in db and show it match

--- front-end/src/users/components/User.js
+++ front-end/src/users/components/User.js
@@ -2,8 +2,7 @@
 import React, { Component } from "react";
 import Posts from "../../posts/component/posts";
 import UserForm from "./UserForm";
-import { getAllUsers} from "../api";
-
+import { getAllUsers } from "../api";
 
 export default class User extends Component {
   constructor(props) {
@@ -11,27 +10,42 @@ export default class User extends Component {
     //create state for empty input by false and create an arry for users
     this.state = {
       UserLog: false,
-      users:[],
+      users: []
     };
   }
-//get all users from API 
+  //get all users from API
   componentDidMount() {
     getAllUsers()
-        .then(response => {
-            this.setState({
-              //fetch the data from the arry in response 
-              users: response.data.users
-            });
-            console.log("result",response.data.users)
-        })
-        //if there  any error 
-        .catch(err => console.log(err));
+      .then(response => {
+        this.setState({
+          //fetch the data from the arry in response
+          users: response.data.users
+        });
+        console.log("result", response.data.users);
+      })
+      //if there  any error
+      .catch(err => console.log(err));
   }
-
+  //create method login
   UserLog = name => {
-    this.setState({
-      UserLog: true,
-    });
+    const users = this.state.users;
+
+    // find the selected name that enter by user
+    const selectedUsersName = users.find(
+      user => user.name.toLowerCase() === name.toLowerCase()
+    );
+    //check if the names found
+    if (selectedUsersName) {
+      //create setStete if found return true
+      this.setState({
+        UserLog: true
+      });
+    } else {
+      //if the name not found return nothing
+      this.setState({
+        UserLog: false
+      });
+    }
   };
   render() {
     //Create varible to check the method UserLog if true display else nothing
@@ -41,7 +55,7 @@ export default class User extends Component {
       ""
     );
     return (
-      //send the method UserLog to userform and display the post 
+      //send the method UserLog to userform and display the post
       <div>
         <UserForm UserLog={this.UserLog} />
         {posts}
