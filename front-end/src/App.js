58a4ab2452b7d06efcefda87b0894727fa36ae41
hayaAbdulaziz <hayal5066@gmail.com>
2020-03-15 19:42:48 +0300

    install react-router-dom and import  in app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import apiURL from './apiConfig';
-// import Organization from './organizations/components/Organization';
-// import User from './users/components/User';
+import Organization from './organizations/components/Organization';
+import User from './users/components/User';
 
 class App extends React.Component {
     //Creat constructor
@@ -22,13 +28,43 @@ class App extends React.Component {
  
   render() {
     return(
-      <div className ="App">
-        {/* <Organization posts={this.state.posts} setPosts={this.setPosts}/> */}
-        {/* <User posts={this.state.posts} setPosts={this.setPosts}/> */}
+      <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/User">User</Link>
+        <Link to="/Organization">Organization</Link>
+      </nav>
 
+      <div>
+        <Route
+          path="/User"
+          render={() => (
+            <User
+            posts={this.state.posts} setPosts={this.setPosts}
+            />
+          )}
+        />
+
+        <Route
+          path="/Organization"
+          render={() => (
+            <Organization
+            posts={this.state.posts} setPosts={this.setPosts}
+            />
+          )}
+        />
       </div>
-        );
-      }
-      }
+    </Router>
+  );
+}
+}
+      
+      // <div className ="App">
+      //   <Organization posts={this.state.posts} setPosts={this.setPosts}/>
+      //   {/* <User posts={this.state.posts} setPosts={this.setPosts}/> */}
+
+      // </div>
+      //   );
+     
       
 export default App;
